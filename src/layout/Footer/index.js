d3f3fc59287f0b846c03f8cbaab2475a505bf5ec
Caleb Olojo <belac335@gmail.com>
2021-01-01 21:17:16 +0100

    refactor footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import footStyle from './style/footer.module.scss'
+import { BiCopyright } from 'react-icons/bi'
 
 const Footer = () => {
+  const date = new Date().getFullYear()
+
   return (
     <footer className={footStyle.root}>
       <div className={footStyle.copyright}>
         <p>
-          Created By Devs for Everyone
+          Created By <a href="https://twitter.com/calebolojo">aoa</a> for
+          Everyone <BiCopyright /> {date}
         </p>
       </div>
     </footer>
