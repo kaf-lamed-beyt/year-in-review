5c762e38364bdb1b2dc451f01be6bf5e6cd76faf
Caleb <belac335@gmail.com>
2022-01-02 21:10:48 +0100

    add meta tags for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,30 @@ function MyApp({ Component, pageProps }) {
         <title>Awesome Year in Reviews</title>
         <link rel="icon" type="image/ico" href="/robocon.png" />
         <meta name="theme-color" content="rgba(225, 18, 51)" />
+        <meta
+          name="description"
+          content="A gallery of year-in review articles written by folks in the tech community"
+        />
+        <meta
+          property="og:title"
+          content="Year in Review Gallery"
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content="A gallery of year-in review articles written by folks in the tech community"
+          key="ogdesc"
+        />
+        <meta
+          property="og:image"
+          content={`https://year-in-review.netlify.app/img/review.png`}
+          key="ogimage"
+        />
+        <meta
+          property="og:site_name"
+          content="https://year-in-review.netlify.app"
+          key="ogsitename"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Monoton&family=Ubuntu:wght@300;400;500&display=swap"
           rel="stylesheet"
